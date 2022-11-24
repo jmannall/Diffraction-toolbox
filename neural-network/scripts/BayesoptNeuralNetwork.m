bc216f61075c408e62e68abd35dd91a28d13b6da
jmannall <110119245+jmannall@users.noreply.github.com>
2022-11-24 09:23:54 +0000

    reduce numEpochs for testing

--- neural-network/scripts/BayesoptNeuralNetwork.m
+++ neural-network/scripts/BayesoptNeuralNetwork.m
@@ -18,7 +18,7 @@ function BayesoptNeuralNetwork(lossFunc, networkSize, numOutputs, controlparamet
     [~, ~, ~, ~, ~, idx, saveData] = CreateBtmTrainingData(epochSize, controlparameters, epochSize);
     dataFunc = @() CreateBtmTrainingData(epochSize, controlparameters, idx);
 
-    numEpochs = 100;
+    numEpochs = 5;
     func = @(x)CreateBTMNeuralNetwork(x, lossFunc, dataFunc, networkSize, numOutputs, numEpochs);
     restarting = isfile(saveResult);
     if restarting
