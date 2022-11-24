f35aa1d2034bd7e073cedadd58e47c206162a398
jmannall <110119245+jmannall@users.noreply.github.com>
2022-11-24 21:55:42 +0000

    Select gpu device in GenerateNNBtmData

--- neural-network/GenerateNNBtmData.m
+++ neural-network/GenerateNNBtmData.m
@@ -9,6 +9,8 @@ nfft = 16384;
 c = 344;
 controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
 
+gpuDevice(1)
+
 for i = 1:numEpochs
     [trainingData, targetData] = CreateBtmTrainingData(epochSize, controlparameters, i);
 end
