218ceaf5b4b49ffca1eb74323ab56b4cb83aa014
jmannall <110119245+jmannall@users.noreply.github.com>
2022-12-09 22:09:38 +0000

    Add dlarray support

--- neural-network/scripts/IIRFilterParameterCoefficients.m
+++ neural-network/scripts/IIRFilterParameterCoefficients.m
@@ -1,9 +1,14 @@
 function [b, a] = IIRFilterParameterCoefficients(lpFc, hsFc, G, k, fs)
 
-    [b, a] = deal(zeros(2, 2, length(lpFc)));
+    isDlarray = isdlarray(lpFc);
+
+    if isDlarray
+        [b, a] = deal(dlarray(zeros(2, 2, length(lpFc))));
+    else
+        [b, a] = deal(zeros(2, 2, length(lpFc)));
+    end
     [b(:,1,:), a(:,1,:)] = LowPassCoefficients(lpFc, fs, k);
     [b(:,2,:), a(:,2,:)] = HighShelfCoefficients(hsFc, G, fs);
-
     b = real(b);
     a = real(a);
     if isDlarray
