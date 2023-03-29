c1e1da3172031cc4ac5c0355b6d19470d23060e3
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 15:02:47 +0100

    More robust folder exists check

--- common/CheckFileDir.m
+++ common/CheckFileDir.m
@@ -1,7 +1,11 @@
 function CheckFileDir(fileName)
 
-    check = exist([cd filesep, fileName], 'dir');
+    check = exist([cd filesep, fileName], 'dir');   % Returns 7 if folder exists and MATLAB can access it
+    
     if check == 0
+        listing = dir(fileName);
+        if isempty(listing)
            mkdir(fileName)
+        end
     end
 end
