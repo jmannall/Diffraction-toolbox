0cef8feab6b6c9656ca587c98ba73ac23d92c3d2
jmannall <110119245+jmannall@users.noreply.github.com>
2023-03-29 14:33:32 +0100

    Add noDirect

--- neural-network/GenerateNNDataSingle.m
+++ neural-network/GenerateNNDataSingle.m
@@ -13,7 +13,7 @@ function GenerateNNDataSingle(size, fs)
     
     nfft = 16384;
     c = 344;
-    controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2);
+    controlparameters = struct('fs', fs, 'nfft', nfft, 'difforder', 1, 'c', c, 'saveFiles', 2, 'noDirect', true);
     
     epochSize = 20e3;
     [~, ~] = CreateBtmTrainingData(epochSize, controlparameters);
