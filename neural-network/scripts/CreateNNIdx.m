bc0f979b6187870200ed5164a4bae11b7fe1e870
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-02 23:40:10 +0100

    DataHash disp test 2

--- neural-network/scripts/CreateNNIdx.m
+++ neural-network/scripts/CreateNNIdx.m
@@ -1,8 +1,12 @@
 function idx = CreateNNIdx(hP, tP, nP)
     nP = rmfield(nP, 'seed');
     nP = rmfield(nP, 'savePath');
+    % idx = DataHash({hP, tP, nP});
+    disp(['tP Loss: ' DataHash({tP.lossFunc})])
+    disp(['tP Data: ' DataHash({tP.dataFunc})])
+    disp(['tP Test: ' DataHash({tP.testFunc})])
+    tP = rmfield(tP, 'lossFunc');
+    tP = rmfield(tP, 'dataFunc');
+    tP = rmfield(tP, 'testFunc');
     idx = DataHash({hP, tP, nP});
-    disp(['hP: ' DataHash({hP})])
-    disp(['tP: ' DataHash({tP})])
-    disp(['nP: ' DataHash({nP})])
 end
