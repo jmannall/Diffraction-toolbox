ecf4c3cf16f5d0b7e227f8bdba034ddad5e52c5c
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-02 23:32:38 +0100

    DataHash disp test

--- neural-network/scripts/CreateNNIdx.m
+++ neural-network/scripts/CreateNNIdx.m
@@ -2,4 +2,7 @@ function idx = CreateNNIdx(hP, tP, nP)
     nP = rmfield(nP, 'seed');
     nP = rmfield(nP, 'savePath');
     idx = DataHash({hP, tP, nP});
+    disp(['hP: ' DataHash({hP})])
+    disp(['tP: ' DataHash({tP})])
+    disp(['nP: ' DataHash({nP})])
 end
--- neural-network/scripts/FixTempNN.m
+++ neural-network/scripts/FixTempNN.m
@@ -2,7 +2,7 @@ close all
 clear all
 
 directory = 'tempNN';
-saveDirectory = 'tempNNNew';
+saveDirectory = 'tempNN';
 CheckFileDir(saveDirectory)
 dirInfo = dir(directory);
 dirInfo = dirInfo(3:end);
