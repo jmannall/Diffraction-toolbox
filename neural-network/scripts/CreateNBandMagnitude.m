e7f965172b97d896f638e6a9a90e733d6825e624
jmannall <110119245+jmannall@users.noreply.github.com>
2023-04-20 15:01:23 +0100

    Return dlarray check

--- neural-network/scripts/CreateNBandMagnitude.m
+++ neural-network/scripts/CreateNBandMagnitude.m
@@ -1,12 +1,11 @@
 function tfmagNBand = CreateNBandMagnitude(tfmag, fidx)
     numBands = max(fidx);
     numObservations = size(tfmag, 2);
-%     if isdlarray(tfmag)
-%         tfmagNBand = dlarray(zeros(numBands, numObservations));
-%     else
-%         tfmagNBand = zeros(numBands, numObservations);
-%     end
-    tfmagNBand = zeros(numBands, numObservations);
+    if isdlarray(tfmag)
+        tfmagNBand = dlarray(zeros(numBands, numObservations));
+    else
+        tfmagNBand = zeros(numBands, numObservations);
+    end
     for i = 1:numBands
         num = sum(fidx == i);
         tfmagNBand(i,:) = 20*log(abs(sum(10 .^ (tfmag(fidx == i,:) / 20), 1) / num)) / log(10);
